d4ce0164e111a5ef3051705a89a13f4c41c99195
Jogenist <57068158+Jogenist@users.noreply.github.com>
2021-05-14 21:25:22 +0200

    added elastic distortion & random erasing augmentations

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ plt.rcParams['image.cmap'] = 'gray'
 plt.rcParams['image.interpolation'] = 'nearest'
 from scipy.ndimage import affine_transform
 from scipy.ndimage.interpolation import geometric_transform
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
 from scipy.ndimage import shift
 from rotations import x_rotmat  # from rotations.py
 from rotations import y_rotmat  # from rotations.py
@@ -27,7 +29,10 @@ Lab_str_lst = []  # Laboratory Structure List?
 # ----------------------------------------------------------------------------
 # Select augmentation here:
 
-Aug_Whtlst = ["rotate", "scale", "flip", "translate", "skew", "blur", "crop&resize" "no augmentation"]
+Aug_Whtlst = ["rotate", "scale", "flip", "translate", "skew", "blur", "crop&resize", "elastic distortion",
+              "random erasing", "no augmentation"]
+
+
 
 # ----------------------------------------------------------------------------
 # load all nii image files from datadir in dataset
@@ -39,9 +44,16 @@ for nii in os.listdir(os.getcwd()):
     nii_data = img.get_fdata()  # get the data as an float array
     # print(nii_data)
     I = nii_data[..., 0]  # I is the first volume
-    # print(I)
+    print(I.shape)
     Img_dataset.append(I)
 Img_dataset = np.asarray(Img_dataset)
+# for i in range(len(Img_dataset)):
+#     print(i)
+#     plt.imshow(Img_dataset[i][:, 10])
+#     plt.show()
+#     # new_image = nib.Nifti1Image(Img_dataset[i], affine=np.eye(4))
+#     # nib.save(new_image,"nii")
+print("------------------------------------")
 
 # load all nii segmentation files from datadir in dataset
 os.chdir("..")  # go back one directory
@@ -93,6 +105,13 @@ def augmentation(aug_int, file_int):
     # -------------------------------------ROTATE-----------------------------------------------
     if aug_int == 0:
         print("---rotate---")
+        """
+        Simple Image Rotation as compared in [O'Gara2019].
+
+
+        [O'Gara2019] O'Gara, McGuinness, "Comparing Data Augmentation Strategies for Deep Image Classification", in
+        IMVIP 2019 Irish Machine Vision and Image Procession, 2019.       
+        """
         angle = rm.uniform(0.1, 0.5)
         print(angle)
         i = 10  # start at layer 10
@@ -107,7 +126,6 @@ def augmentation(aug_int, file_int):
         plt.show()
         i = i + 1
         aug_str = "rotated"
-
     # -------------------------------------SCALE-----------------------------------------------
     if aug_int == 1:
         print("---scale---")
@@ -192,15 +210,17 @@ def augmentation(aug_int, file_int):
         plt.show()
         i = i + 1
         aug_str = "blured"
-    # -------------------------------------Random Crop and Rezize-----------------------------------------------
+    # -------------------------------------RANDOM CROP AND RESIZE-----------------------------------------------
     if aug_int == 6:
         print("---Crop & Resize---")
         i = 10  # start at layer 10
         K = np.empty(Block_Size)
         P = np.empty(Block_Size)
+        a = rm.randrange(0, 48)  #randomly select starting point of crop
+        print("a:", a)
         for n in range(Img_dataset[file_int].shape[2]):         # go through each nifty slice
-            crop_k = Img_dataset[file_int][16:48,16:48, n]
-            crop_p = Img_dataset[file_int][16:48, 16:48, n]
+            crop_k = Img_dataset[file_int][a:a+16, a:a+16, n]     #crop size is always 16x16
+            crop_p = Img_dataset[file_int][a:a+16, a:a+16, n]
             crop_resize_k = resize(crop_k, (64,64))
             crop_resize_p = resize(crop_p, (64,64))
             K[:, :, n] = crop_resize_k                            # append skewed slice to numpy array (image)
@@ -209,9 +229,73 @@ def augmentation(aug_int, file_int):
         plt.imshow(K[:, i])
         plt.show()
         i = i + 1
-        aug_str = "crop & resize"
-    # -------------------------------------NO AUGMENTATION-----------------------------------------------
+        aug_str = "crop_resize"
+    # -------------------------------------ELASTIC DISTORTION-----------------------------------------------
     if aug_int == 7:
+        print("---Elastic Distortion---")
+        """
+        Elastic deformation as described in [Simard2003].
+
+
+        [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
+        Convolutional Neural Networks applied to Visual Document Analysis", in
+        Proc. of the International Conference on Document Analysis and
+        Recognition, 2003.
+        """
+        i = 10  # start at layer 10
+        K = np.empty(Block_Size)
+        P = np.empty(Block_Size)
+        # ----- use this settings to create art ------
+        # alpha = 20
+        # sigma = 1
+        # --------------------------------------------
+        alpha = 55
+        sigma = 10
+        # alpha = rm.randrange(1, 10)  # randomly select starting point of crop
+        print("alpha:", alpha)
+        #sigma = rm.randrange(1,10)
+        print("sigma: ", sigma)
+        random_state = np.random.RandomState(None)
+        for n in range(Img_dataset[file_int].shape[2]):  # go through each nifty slice
+            shape = Img_dataset[file_int][n].shape
+            dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+            dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+            x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), indexing='ij')
+            indices = np.reshape(x + dx, (-1, 1)), np.reshape(y + dy, (-1, 1))
+            K[:, :, n] = map_coordinates(Img_dataset[file_int][n], indices, order=1).reshape(shape)  # append distorted slice to numpy array (image)
+            P[:, :, n] = map_coordinates(Lab_dataset[file_int][n], indices, order=1).reshape(shape)  # append distorted slice to numpy array (label)
+        print(K.shape)
+        plt.imshow(K[:,:, i])
+        plt.show()
+        i = i + 1
+        aug_str = "elastic_distortion"
+    # -------------------------------------RANDOM ERASING-----------------------------------------------
+    if aug_int == 8:
+        print("---Random Erasing---")
+        """
+        Random Erasing as compared in [O'Gara2019].
+
+
+        [O'Gara2019] O'Gara, McGuinness, "Comparing Data Augmentation Strategies for Deep Image Classification", in
+        IMVIP 2019 Irish Machine Vision and Image Procession, 2019.
+        """
+        i = 10  # start at layer 10
+        K = np.empty(Block_Size)
+        P = np.empty(Block_Size)
+        a = rm.randrange(0, 48)                         # randomly select pixels to be erased
+        b = 10                                          # set size of erased area here
+        for n in range(Img_dataset[file_int].shape[2]):  # go through each nifty slice
+            Img_dataset[file_int][a:a+b, a:a+b, n] = 0
+            Lab_dataset[file_int][a:a+b, a:a+b, n] = 0
+            K[:, :, n] = Img_dataset[file_int][:, :, n]
+            P[:, :, n] = Lab_dataset[file_int][:, :, n]
+        print(K.shape)
+        plt.imshow(K[:, :, i])
+        plt.show()
+        i = i + 1
+        aug_str = "random_erasing"
+    # -------------------------------------NO AUGMENTATION-----------------------------------------------
+    if aug_int == 9:
         print("---no augmentation---")
         i = 10  # start at layer 10
         K = Img_dataset[file_int]
